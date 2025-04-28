181e3d04b1442cbf6851af1b5ae56e95984e903d
Sadelto0130 <49660985+Sadelto0130@users.noreply.github.com>
2025-04-27 23:39:15 -0500

    vetapp v3 - Timeout server agregado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
+import http from 'http';
 import app from './app.js';
 import { PORT } from './config.js';
 
-// conexion al servidor
-app.listen(PORT)
-console.log('Servidor iniciado!', PORT);
+const server = http.createServer(app);
+
+// Setear timeout a 30 segundos
+server.setTimeout(30000, (socket) => {
+  console.log('Server timeout!');
+
+  // Opcional: puedes destruir la conexión si quieres
+  socket.destroy();
+});
+
+// Ahora iniciar el servidor
+server.listen(PORT, () => {
+  console.log('Servidor iniciado en el puerto', PORT);
+});
