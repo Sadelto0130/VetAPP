3dc88f86864b10b01a8c11fd1bbfc5481050c911
Sadelto0130 <49660985+Sadelto0130@users.noreply.github.com>
2025-04-27 23:11:50 -0500

    vetapp v3

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export const PG_USER = process.env.USER || "postgres";
 export const PG_PASSWORD = process.env.PASSWORD || "password";
 export const PG_DATABASE = process.env.DATABASE || "veterinaria";
 
-export const ORIGIN_CORS = process.env.ORIGIN || "http://localhost:5173"
+export const ORIGIN = process.env.ORIGIN || "http://localhost:5173"
 
 export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME || "dpiubglwk"
 export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY || 287286962684634
