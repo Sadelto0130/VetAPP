2eed4975fede25ec5279c83b6a7c30d69e5a9720
Sadelto0130 <49660985+Sadelto0130@users.noreply.github.com>
2025-05-16 22:35:50 -0500

    vetapp v1.3.1 - correccion frontend perfil del usuario

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -41,9 +41,7 @@ function ProfilePage() {
         </div>
       ) : (
         <>
-          {errors ? (
-            <p className="text-red-500 text-center mt-2 mb-1">Errors: {errors}</p>
-          ) : (
+          {(
             <div className="items-center justify-center relative pt-20 ">
               <div className="bg-white/80 relative shadow rounded-lg w-5/6 md:w-5/6  lg:w-4/6 xl:w-3/6 mx-auto pb-6 pl-2 pr-2">
                 <div className="flex justify-center">
