88c0212bb816a7504c8f102ddf3a6bbb3afc3a21
Sadelto0130 <49660985+Sadelto0130@users.noreply.github.com>
2025-05-17 01:12:27 -0500

    vetapp v1.3.5 - correccion mostrar rutas

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -31,8 +31,7 @@ function ProfilePage() {
     <>
       {loading ? (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-white">
-            Loading...
+          <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-white">            
           </div>
         </div>
       ) : (
